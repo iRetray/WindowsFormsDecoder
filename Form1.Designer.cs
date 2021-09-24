8d85a69b2b611bac0113c8d13eb891cadfc6caed
Julian Camilo <julian20141016@gmail.com>
2021-09-23 20:52:16 -0500

    Creado diseño visual con Windows Forms components

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,22 @@ namespace Decoder {
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label2 = new System.Windows.Forms.Label();
+            this.textToCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.letterToCode = new System.Windows.Forms.ComboBox();
+            this.buttonCode = new System.Windows.Forms.Button();
+            this.codedValue = new System.Windows.Forms.Label();
+            this.buttonDecode = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.decodeValue = new System.Windows.Forms.Label();
+            this.letterToDecode = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textToDecode = new System.Windows.Forms.TextBox();
             this.Decodificador.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -48,35 +64,54 @@ namespace Decoder {
             // 
             this.Decodificador.Controls.Add(this.tabPage1);
             this.Decodificador.Controls.Add(this.tabPage2);
+            this.Decodificador.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Decodificador.Location = new System.Drawing.Point(12, 12);
             this.Decodificador.Name = "Decodificador";
             this.Decodificador.SelectedIndex = 0;
-            this.Decodificador.Size = new System.Drawing.Size(974, 626);
+            this.Decodificador.Size = new System.Drawing.Size(493, 450);
             this.Decodificador.TabIndex = 2;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.decodeValue);
+            this.tabPage1.Controls.Add(this.letterToDecode);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.textToDecode);
+            this.tabPage1.Controls.Add(this.buttonDecode);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.textBox2);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(966, 600);
+            this.tabPage1.Size = new System.Drawing.Size(485, 424);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Decodificador";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.codedValue);
+            this.tabPage2.Controls.Add(this.buttonCode);
+            this.tabPage2.Controls.Add(this.letterToCode);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textToCode);
             this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage2.ForeColor = System.Drawing.Color.Black;
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(966, 600);
+            this.tabPage2.Size = new System.Drawing.Size(485, 424);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Codificador";
             this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
             // label2
             // 
@@ -88,11 +123,250 @@ namespace Decoder {
             this.label2.TabIndex = 2;
             this.label2.Text = "Codificador";
             // 
+            // textToCode
+            // 
+            this.textToCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textToCode.Location = new System.Drawing.Point(36, 97);
+            this.textToCode.Name = "textToCode";
+            this.textToCode.Size = new System.Drawing.Size(290, 30);
+            this.textToCode.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Texto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(335, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Letra clave";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // letterToCode
+            // 
+            this.letterToCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letterToCode.FormattingEnabled = true;
+            this.letterToCode.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.letterToCode.Location = new System.Drawing.Point(340, 97);
+            this.letterToCode.Name = "letterToCode";
+            this.letterToCode.Size = new System.Drawing.Size(121, 28);
+            this.letterToCode.TabIndex = 6;
+            this.letterToCode.Text = "A";
+            // 
+            // buttonCode
+            // 
+            this.buttonCode.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCode.ForeColor = System.Drawing.Color.White;
+            this.buttonCode.Location = new System.Drawing.Point(36, 148);
+            this.buttonCode.Name = "buttonCode";
+            this.buttonCode.Size = new System.Drawing.Size(111, 36);
+            this.buttonCode.TabIndex = 7;
+            this.buttonCode.Text = "Codificar";
+            this.buttonCode.UseVisualStyleBackColor = false;
+            // 
+            // codedValue
+            // 
+            this.codedValue.AutoSize = true;
+            this.codedValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codedValue.Location = new System.Drawing.Point(36, 216);
+            this.codedValue.Name = "codedValue";
+            this.codedValue.Size = new System.Drawing.Size(0, 25);
+            this.codedValue.TabIndex = 8;
+            // 
+            // buttonDecode
+            // 
+            this.buttonDecode.BackColor = System.Drawing.Color.RoyalBlue;
+            this.buttonDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecode.ForeColor = System.Drawing.Color.White;
+            this.buttonDecode.Location = new System.Drawing.Point(36, 149);
+            this.buttonDecode.Name = "buttonDecode";
+            this.buttonDecode.Size = new System.Drawing.Size(126, 36);
+            this.buttonDecode.TabIndex = 13;
+            this.buttonDecode.Text = "Decodificar";
+            this.buttonDecode.UseVisualStyleBackColor = false;
+            this.buttonDecode.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.comboBox2.Location = new System.Drawing.Point(340, 98);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 12;
+            this.comboBox2.Text = "A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(335, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Letra clave";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(31, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Texto";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(36, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(290, 30);
+            this.textBox2.TabIndex = 9;
+            // 
+            // decodeValue
+            // 
+            this.decodeValue.AutoSize = true;
+            this.decodeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decodeValue.Location = new System.Drawing.Point(36, 217);
+            this.decodeValue.Name = "decodeValue";
+            this.decodeValue.Size = new System.Drawing.Size(0, 25);
+            this.decodeValue.TabIndex = 20;
+            // 
+            // letterToDecode
+            // 
+            this.letterToDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letterToDecode.FormattingEnabled = true;
+            this.letterToDecode.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.letterToDecode.Location = new System.Drawing.Point(340, 98);
+            this.letterToDecode.Name = "letterToDecode";
+            this.letterToDecode.Size = new System.Drawing.Size(121, 28);
+            this.letterToDecode.TabIndex = 18;
+            this.letterToDecode.Text = "A";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(335, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Letra clave";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(31, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Texto";
+            // 
+            // textToDecode
+            // 
+            this.textToDecode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textToDecode.Location = new System.Drawing.Point(36, 98);
+            this.textToDecode.Name = "textToDecode";
+            this.textToDecode.Size = new System.Drawing.Size(290, 30);
+            this.textToDecode.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(998, 650);
+            this.ClientSize = new System.Drawing.Size(512, 471);
             this.Controls.Add(this.Decodificador);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -112,6 +386,22 @@ namespace Decoder {
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textToCode;
+        private System.Windows.Forms.ComboBox letterToCode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonCode;
+        private System.Windows.Forms.Label codedValue;
+        private System.Windows.Forms.Button buttonDecode;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label decodeValue;
+        private System.Windows.Forms.ComboBox letterToDecode;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textToDecode;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,17 @@ namespace Decoder {
         private void label1_Click(object sender, EventArgs e) {
 
         }
+
+        private void label4_Click(object sender, EventArgs e) {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+
+        }
     }
 }
